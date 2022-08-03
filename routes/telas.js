8629f1587b8940bcf22ec8faf22f1069531a1f58
efrahim Tamaku <efrahimtks@gmail.com>
2022-08-03 15:22:51 +0100

    Começando a fazer as paginas dos cursos

--- routes/telas.js
+++ routes/telas.js
@@ -1,10 +1,17 @@
-const roteador = require("express").Router()
+const { Router } = require("express")
+
+const roteador = Router()
 
 roteador.get("/", (req, res) => {
     res.render("index")
 })
-roteador.get("/curso-de-eletricidade", (req, res)=>{
-    res.render("curso-de-eletricidade")
+
+//rotas cursos
+const rotasCursos = Router()
+rotasCursos.get("/eletricidade-baixa-tensao", (req, res) => {
+    res.status(200).render("eletricidade-baixa-tensao")
 })
+
+roteador.use("/cursos", rotasCursos)
 
 module.exports = roteador
