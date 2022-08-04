d6be6c4fe344c9d9971fd2e4af61e7c923fc4117
Nelson0Simao <nelsonsimao100@gmail.com>
2022-08-04 14:13:46 +0100

    add rotas

--- routes/telas.js
+++ routes/telas.js
@@ -11,6 +11,15 @@ const rotasCursos = Router()
 rotasCursos.get("/eletricidade-baixa-tensao", (req, res) => {
     res.status(200).render("eletricidade-baixa-tensao")
 })
+rotasCursos.get("/nome-do-curso-1", (req, res) => {
+    res.status(200).render("route-1")
+})
+rotasCursos.get("/nome-do-curso-2", (req, res) => {
+    res.status(200).render("route-2")
+})
+rotasCursos.get("/nome-do-curso-3", (req, res) => {
+    res.status(200).render("route-3")
+})
 
 roteador.use("/cursos", rotasCursos)
 
