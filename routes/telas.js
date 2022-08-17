ce17aaa41b6e10ec71017f21049869750e2076d9
Efrahim Tamaku <efrahimtks@gmail.com>
2022-08-17 10:01:26 +0100

    Mais Rotas de cursos criadas

--- routes/telas.js
+++ routes/telas.js
@@ -12,6 +12,21 @@ rotasCursos.get("/eletricidade-baixa-tensao", (req, res) => {
     res.status(200).render("eletricidade-baixa-tensao")
 })
 
+rotasCursos.get("/informatica-tecnica", (req, res) => {
+    res.status(200).render("informatica-tecnica")
+})
+
+rotasCursos.get("/gestao-sistemas", (req, res) => {
+    res.status(200).render("gestao-sistemas")
+})
+
+rotasCursos.get("/metalomecanica-torneiro", (req, res) => {
+    res.status(200).render("metalomecanica-torneiro")
+})
+rotasCursos.get("/maquinas-motores", (req, res) => {
+    res.status(200).render("maquinas-motores")
+})
+
 roteador.use("/cursos", rotasCursos)
 
 export default roteador
