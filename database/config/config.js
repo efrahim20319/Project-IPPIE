818d7fb90b2996afb890a9cbf5f1bb6a0a0440a2
Efrahim Tamaku <efrahimtks@gmail.com>
2023-07-12 11:04:40 +0100

    securely running the db

--- database/config/config.js
+++ database/config/config.js
@@ -1,25 +1,33 @@
-require("dotenv").config()
+require('dotenv').config()
 
 module.exports = {
-  "development": {
-    "username": process.env.DBASE_USER,
-    "password": process.env.DBASE_PASS,
-    "database": process.env.DBASE_NAME,
-    "host": process.env.DBASE_HOST,
-    "dialect": process.env.DBASE_DIALECT
-  },
-  "test": {
-    "username": process.env.DBASE_USER,
-    "password": process.env.DBASE_PASS,
-    "database": process.env.DBASE_NAME,
-    "host": process.env.DBASE_HOST,
-    "dialect": process.env.DBASE_DIALECT
-  },
-  "production": {
-    "username": process.env.DBASE_USER,
-    "password": process.env.DBASE_PASS,
-    "database": process.env.DBASE_NAME,
-    "host": process.env.DBASE_HOST,
-    "dialect": process.env.DBASE_DIALECT
-  }
+    development: {
+        username: process.env.DBASE_USER,
+        password: process.env.DBASE_PASS,
+        database: process.env.DBASE_NAME,
+        host: process.env.DBASE_HOST,
+        dialect: process.env.DBASE_DIALECT,
+    },
+    test: {
+        username: process.env.DBASE_USER,
+        password: process.env.DBASE_PASS,
+        database: process.env.DBASE_NAME,
+        host: process.env.DBASE_HOST,
+        dialect: process.env.DBASE_DIALECT,
+    },
+    production: {
+        username: process.env.DBASE_USER,
+        password: process.env.DBASE_PASS,
+        database: process.env.DBASE_NAME,
+        host: process.env.DBASE_HOST,
+        dialect: process.env.DBASE_DIALECT,
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: true,
+            },
+        },
+        define: {
+            timestamps: false,
+        },
+    },
 }
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-
-import app from "./app";
-import ConexaoListas from "./database/redis/conexaoListas";
-const port = process.env.APP_PORT;
-const db = require("./database/models/index")
+import app from './app'
+import ConexaoListas from './database/redis/conexaoListas'
+const mysql2 = require('mysql2')
+const port = process.env.APP_PORT
+const db = require('./database/models/index')
 
 async function initialize() {
     try {
+        // const connection = mysql2.createConnection(process.env.DATABASE_URL)
+        // console.log('Connected to PlanetScale!')
+        // connection.end()
         await db.sequelize.authenticate();
-        console.log('Connection has been established successfully.');
+        console.log('Connection has been established successfully.')
         // await ConexaoListas()
-        app.listen(port, () => console.log("Server up and running at port", port));
+        app.listen(port, () =>
+            console.log('Server up and running at port', port)
+        )
     } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        console.error('Unable to connect to the database:', error)
     }
 }
 
 initialize()
     .then(() => {
-        console.log("SERVER RUNNING");
+        console.log('SERVER RUNNING')
     })
     .catch((error) => {
-        console.error("SERVER NOT RUNNING", error);
+        console.error('SERVER NOT RUNNING', error)
     })
