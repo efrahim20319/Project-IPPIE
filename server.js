1cb00fde21cc57ed9dea32c18c29ba84f01dac16
Efrahim Tamaku <efrahimtks@gmail.com>
2023-07-12 09:54:02 +0100

    integrando uma base de dados da planet scale

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = require("./database/models/index")
 
 async function initialize() {
     try {
-        // await db.sequelize.authenticate();
+        await db.sequelize.authenticate();
         console.log('Connection has been established successfully.');
         // await ConexaoListas()
         app.listen(port, () => console.log("Server up and running at port", port));
